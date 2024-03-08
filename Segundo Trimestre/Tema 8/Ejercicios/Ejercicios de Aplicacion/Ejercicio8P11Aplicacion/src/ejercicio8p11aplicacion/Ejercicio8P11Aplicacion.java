1a610665eebfdf47478e01a73b7a522c386131f3
Sam170703 <sam170703dev@gmail.com>
2024-03-08 10:42:42 +0100

    Updated Programación branch

---
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios de Aplicacion/Ejercicio8P11Aplicacion/src/ejercicio8p11aplicacion/Ejercicio8P11Aplicacion.java
@@ -0,0 +1,16 @@
+package ejercicio8p11aplicacion;
+
+/**
+ *
+ * @author Samuel Vergara Martín
+ */
+public class Ejercicio8P11Aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios de Aplicacion/Ejercicio8P11Aplicacion/src/ejercicio8p11aplicacion/Nota.java
@@ -0,0 +1,7 @@
+package ejercicio8p11aplicacion;
+
+/**
+ *
+ * @author Samuel Vergara Martín
+ */
+public enum Nota {DO, RE, MI, FA, SOL, LA, SI}
