1ba8bc2e2ce1e9c2e84cabb485632a3ea75f680b
Sam170703 <sam170703dev@gmail.com>
2024-03-08 08:38:25 +0100

    Updated Programación branch

--- Segundo Trimestre/Tema 8/Ejercicios/Ejercicios Resueltos/Ejercicio8P1/src/ejercicio8p1/Ejercicio8P1.java
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios Resueltos/Ejercicio8P1/src/ejercicio8p1/Ejercicio8P1.java
@@ -1,5 +1,6 @@
 package ejercicio8p1;
-
+import static netcat_utils.Colors.CYAN;
+import static netcat_utils.NetCatConsole.printSeparator;
 /**
  *
  * @author Samuel Vergara Martín
@@ -10,7 +11,20 @@ public class Ejercicio8P1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String currentValue;
+        Hora hora = new Hora(10, 20);
+        currentValue = hora.toString();
+        System.out.println(CYAN + currentValue);
+        printSeparator();
+        hora.inc();
+        currentValue = hora.toString();
+        System.out.println(CYAN + currentValue);
+        printSeparator();
+        hora.setHora(20);
+        hora.setMinutos(30);
+        currentValue = hora.toString();
+        System.out.println(CYAN + currentValue);
+        printSeparator();
     }
     
 }
--- Segundo Trimestre/Tema 8/Ejercicios/Ejercicios Resueltos/Ejercicio8P1/src/ejercicio8p1/Hora.java
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios Resueltos/Ejercicio8P1/src/ejercicio8p1/Hora.java
@@ -14,7 +14,32 @@ public class Hora {
     }
     
     void inc() {
-        if(this.minutos == 59) this.minutos = 0;
+        if(this.minutos == 59) {
+            this.minutos = 0;
+            this.horas++;
+        }
         else this.minutos++;
+    }
+    
+    boolean setMinutos(int value) {
+        if(value < 60 || value > 0) {
+            this.minutos = value;
+            return true;
+        }
+        else return false;
+    }
+    
+    boolean setHora(int value) {
+        if(value >= 0 || value <= 23) {
+            this.horas = value;
+            return true;
+        }
+        else return false;
+    }
+    
+    @Override
+    public String toString() {
+        String result = this.horas + ":" + this.minutos;
+        return result;
     }
 }
