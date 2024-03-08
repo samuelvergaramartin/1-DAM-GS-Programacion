fe0cca61bde7dbef79ca01dac06c1dadea2c4281
Sam170703 <sam170703dev@gmail.com>
2024-03-08 09:47:46 +0100

    Updated Programación branch

---
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios Resueltos/Ejercicio8P5/src/ejercicio8p5/Ejercicio8P5.java
@@ -0,0 +1,16 @@
+package ejercicio8p5;
+
+/**
+ *
+ * @author Samuel Vergara Martín
+ */
+public class Ejercicio8P5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
