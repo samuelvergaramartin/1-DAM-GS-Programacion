a6c760da5b5c02c8d280b0f7c115305a488f4412
Sam170703 <sam170703dev@gmail.com>
2024-03-08 09:44:51 +0100

    Updated Programación branch

--- Segundo Trimestre/Tema 8/Ejercicios/Ejercicios Resueltos/Ejercicio8P4/src/ejercicio8p4/Instrumento.java
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios Resueltos/Ejercicio8P4/src/ejercicio8p4/Instrumento.java
@@ -4,6 +4,15 @@ package ejercicio8p4;
  *
  * @author Samuel Vergara Martín
  */
-public class Instrumento {
+public abstract class Instrumento {
+    protected Notas[] melodia;
+    public Instrumento() {
+        melodia = new Notas[0];
+    }
+    void add(Notas nota) {
+        melodia = java.util.Arrays.copyOf(melodia, melodia.length +1);
+        melodia[melodia.length - 1] = nota;
+    }
     
+    abstract void interpretar();
 }
--- Segundo Trimestre/Tema 8/Ejercicios/Ejercicios Resueltos/Ejercicio8P4/src/ejercicio8p4/Notas.java
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios Resueltos/Ejercicio8P4/src/ejercicio8p4/Notas.java
@@ -5,5 +5,5 @@ package ejercicio8p4;
  * @author Samuel Vergara Martín
  */
 public class Notas {
-    
+    public enum Nota {DO, RE, MI, FA, SOL, LA, SI};
 }
