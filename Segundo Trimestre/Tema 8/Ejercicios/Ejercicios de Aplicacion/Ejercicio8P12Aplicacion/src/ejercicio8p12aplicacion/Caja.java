875ac69c9134973a58eae8a1ad4dd4218c889e4d
Sam170703 <sam170703dev@gmail.com>
2024-03-09 20:17:50 +0100

    Updated Programación branch

--- Segundo Trimestre/Tema 8/Ejercicios/Ejercicios de Aplicacion/Ejercicio8P12Aplicacion/src/ejercicio8p12aplicacion/Caja.java
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios de Aplicacion/Ejercicio8P12Aplicacion/src/ejercicio8p12aplicacion/Caja.java
@@ -1,5 +1,6 @@
 package ejercicio8p12aplicacion;
-
+import static netcat_utils.Colors.CYAN;
+import static netcat_utils.Colors.RED;
 /**
  *
  * @author Samuel Vergara Martín
@@ -24,6 +25,13 @@ public class Caja {
     }
     
     void setEtiqueta(String etiqueta) {
-        this.etiqueta = etiqueta;
+        if(etiqueta.length() > 30) System.out.println(RED + "ERROR: Escriba una etiqueta nueva. El limite de carcacteres maximos es 30!");
+        else this.etiqueta = etiqueta;
+    }
+    
+    @Override
+    public String toString() {
+        String resultado = CYAN + "Ancho: " + this.ancho + this.unidad + "\n" + "Alto: " + this.alto + this.unidad + "\n" + "Fondo: " + this.fondo + this.unidad + "\n" + "Etiqueta: " + this.etiqueta;
+        return resultado;
     }
 }
--- Segundo Trimestre/Tema 8/Ejercicios/Ejercicios de Aplicacion/Ejercicio8P12Aplicacion/src/ejercicio8p12aplicacion/Ejercicio8P12Aplicacion.java
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios de Aplicacion/Ejercicio8P12Aplicacion/src/ejercicio8p12aplicacion/Ejercicio8P12Aplicacion.java
@@ -1,4 +1,5 @@
 package ejercicio8p12aplicacion;
+import static netcat_utils.Colors.CYAN;
 
 /**
  *
@@ -10,7 +11,14 @@ public class Ejercicio8P12Aplicacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        double volumenCaja;
+        String etiquetaValue;
+        Caja caja = new Caja(20, 30, 50, Unidad.cm);
+        volumenCaja = caja.getVolumen();
+        System.out.println(CYAN + volumenCaja + " " +  Unidad.m3);
+        caja.setEtiqueta("- Samuel Vergara\n- Avda Ortega");
+        etiquetaValue = caja.toString();
+        System.out.println(etiquetaValue);
     }
     
 }
