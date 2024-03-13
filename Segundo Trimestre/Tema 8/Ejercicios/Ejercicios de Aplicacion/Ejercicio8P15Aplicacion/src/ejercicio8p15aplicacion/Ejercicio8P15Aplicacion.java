2a9942a6ef7fcf1cf7e7aa746c8262f00a708a25
Sam170703 <sam170703dev@gmail.com>
2024-03-13 08:55:37 +0100

    Updated Programación branch

---
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios de Aplicacion/Ejercicio8P15Aplicacion/src/ejercicio8p15aplicacion/Ejercicio8P15Aplicacion.java
@@ -0,0 +1,19 @@
+package ejercicio8p15aplicacion;
+
+/**
+ *
+ * @author Samuel Vergara Martín
+ */
+public class Ejercicio8P15Aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String algo = "algo";
+        boolean test;
+        Lista lista = new Lista();
+        test = lista.equals(lista, 2);
+    }
+    
+}
---
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios de Aplicacion/Ejercicio8P15Aplicacion/src/ejercicio8p15aplicacion/Lista.java
@@ -0,0 +1,101 @@
+package ejercicio8p15aplicacion;
+import java.util.Arrays;
+import static netcat_utils.Colors.CYAN;
+import static netcat_utils.Colors.RED;
+/**
+ *
+ * @author Samuel Vergara Martín
+ */
+public class Lista {
+    private Integer[] tabla;
+    
+    Lista() {
+        this.tabla = new Integer[0];
+    }
+    
+    int getTotalElementos() {
+        return this.tabla.length;
+    }
+    
+    void setNumberToEndList(Integer numero) {
+        Integer resolve[] = new Integer[this.tabla.length + 1];
+        System.arraycopy(this.tabla, 0, resolve, 0, resolve.length -1);
+        this.tabla = null;
+        this.tabla = new Integer[resolve.length];
+        System.arraycopy(resolve, 0, this.tabla, 0, this.tabla.length - 1);
+        resolve = null;
+        this.tabla[tabla.length - 1] = numero;
+    }
+    
+    void setNumberToStartList(Integer numero) {
+        int oldLengthTable = this.tabla.length;
+        Integer resolve[] = new Integer[this.tabla.length + 1];
+        System.arraycopy(this.tabla, 0, resolve, 0, resolve.length -1);
+        this.tabla = null;
+        this.tabla = new Integer[resolve.length];
+        System.arraycopy(resolve, 0, this.tabla, 0, this.tabla.length - 1);
+        resolve = null;
+        int lengthTable = this.tabla.length;
+        Integer temp[] = new Integer[oldLengthTable];
+        System.arraycopy(this.tabla, 0, temp, 0, temp.length);
+        this.tabla = null;
+        this.tabla = new Integer[lengthTable];
+        System.arraycopy(temp, 0, this.tabla, 1, this.tabla.length - 1);
+        this.tabla[0] = numero;
+    }
+    
+    void insertNumberToListByIndex(Integer numero, int index) {
+        this.tabla[index] = numero;
+    }
+    
+    void insertOtherTableToEndList(Integer[] otherTable) {
+        int position = this.tabla.length;
+        Integer resolve[] = new Integer[this.tabla.length + otherTable.length];
+        System.arraycopy(this.tabla, 0, resolve, 0, position);
+        this.tabla = null;
+        this.tabla = new Integer[resolve.length];
+        System.arraycopy(resolve, 0, this.tabla, 0, this.tabla.length - 1);
+        resolve = null;
+        System.arraycopy(otherTable, 0, this.tabla, position, this.tabla.length - 2);
+    }
+    
+    void deleteElementForTableByIndex(int index) {
+        this.tabla[index] = null;
+    }
+    
+    Integer getElementFromTableByIndex(int index) {
+        return this.tabla[index];
+    }
+    
+    void searchElementInTable(Integer element) {
+        int position = -1;
+        boolean control = false;
+        for(int i = 0; i < this.tabla.length; i++) {
+            if(this.tabla[i] == element && control == false) {
+                control = true;
+                position = i;
+            }
+        }
+        if(position < 0) System.out.println(CYAN + -1);
+        else System.out.println(CYAN + position);
+    }
+    
+    boolean equals(Lista lista, Object algo) {
+        try {
+            String test = "";
+            boolean te = algo.getClass().equals(test.getClass());
+            System.out.println(te);
+            return false;
+        }
+        catch (ArrayIndexOutOfBoundsException exception) {
+            System.out.println(RED + "Error: La longitud de ambos arrays son distintas entre si.");
+            return false;
+        }
+    }
+    
+    @Override
+    public String toString() {
+        String result = CYAN + Arrays.toString(this.tabla);
+        return result;
+    }
+}
