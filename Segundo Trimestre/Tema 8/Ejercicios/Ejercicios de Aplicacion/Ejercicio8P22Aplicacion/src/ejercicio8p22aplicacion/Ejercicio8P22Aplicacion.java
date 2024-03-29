45de351f53197586e326bf140abb100435851a58
Sam170703 <sam170703dev@gmail.com>
2024-03-29 14:55:07 +0100

    Updated Programación branch

---
+++ Segundo Trimestre/Tema 8/Ejercicios/Ejercicios de Aplicacion/Ejercicio8P22Aplicacion/src/ejercicio8p22aplicacion/Ejercicio8P22Aplicacion.java
@@ -0,0 +1,16 @@
+package ejercicio8p22aplicacion;
+
+/**
+ *
+ * @author Samuel Vergara Martín
+ */
+public class Ejercicio8P22Aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
