29469573ab7ede74f56a760a7825367a79614602
Sam170703 <sam170703dev@gmail.com>
2024-04-01 13:30:39 +0200

    Updated Programación branch

---
+++ Segundo Trimestre/Tema 9/Ejemplos/Example1/src/example1/Perro.java
@@ -0,0 +1,12 @@
+package example1;
+
+/**
+ *
+ * @author Samuel Vergara Martín
+ */
+public class Perro implements Sonido{
+    @Override
+    public void voz() {
+        System.out.println("Guau!");
+    }
+}
---
+++ Segundo Trimestre/Tema 9/Ejemplos/Example1/src/example1/Triangulo.java
@@ -0,0 +1,16 @@
+
+package example1;
+
+/**
+ *
+ * @author Samuel Vergara Martin
+ */
+public class Triangulo implements Sonido{
+     @Override
+     public void voz() {
+         System.out.println("Cling!");
+     }
+     public void vozDurmiendo() {
+         System.out.println("");
+     }
+}
