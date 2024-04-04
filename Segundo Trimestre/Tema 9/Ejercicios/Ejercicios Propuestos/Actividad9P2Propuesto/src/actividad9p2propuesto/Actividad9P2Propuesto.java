e18461d578518150f0a2e584d3dde4f445ae188d
Sam170703 <sam170703dev@gmail.com>
2024-04-04 08:54:54 +0200

    Updated Programación branch

---
+++ Segundo Trimestre/Tema 9/Ejercicios/Ejercicios Propuestos/Actividad9P2Propuesto/src/actividad9p2propuesto/Actividad9P2Propuesto.java
@@ -0,0 +1,16 @@
+package actividad9p2propuesto;
+
+/**
+ *
+ * @author Samuel Vergara Martín
+ */
+public class Actividad9P2Propuesto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
