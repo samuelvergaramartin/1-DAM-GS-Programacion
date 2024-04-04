533eea10268660b0298ca908e12857d1af56f338
Sam170703 <sam170703dev@gmail.com>
2024-04-04 09:19:56 +0200

    Updated Programación branch

---
+++ Segundo Trimestre/Tema 9/Ejercicios/Ejercicios Propuestos/Actividad9P2Propuesto/src/actividad9p2propuesto/ColaDoble.java
@@ -0,0 +1,10 @@
+package actividad9p2propuesto;
+
+/**
+ *
+ * @author Samuel Vergara Martín
+ */
+public interface ColaDoble extends Cola{
+    void EncolarCabeza(Integer nuevo);
+    Integer desencolarFinal();
+}
