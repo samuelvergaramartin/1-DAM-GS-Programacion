886316774a530c5896b3a062d2a8f3c59f0226f3
Sam170703 <sam170703dev@gmail.com>
2024-04-06 20:11:03 +0200

    Updated Programación branch

--- Segundo Trimestre/Tema 9/Ejercicios/Ejercicios Propuestos/Actividad9P2Propuesto/src/actividad9p2propuesto/Actividad9P2Propuesto.java
+++ Segundo Trimestre/Tema 9/Ejercicios/Ejercicios Propuestos/Actividad9P2Propuesto/src/actividad9p2propuesto/Actividad9P2Propuesto.java
@@ -10,7 +10,7 @@ public class Actividad9P2Propuesto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hello world");
     }
     
 }
