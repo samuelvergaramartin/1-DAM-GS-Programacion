a700c67439bf7c024a9fb524c993b91179d6422b
Sam170703 <sam170703dev@gmail.com>
2024-04-07 20:02:21 +0200

    Updated Programación branch

---
+++ Nueva Carpeta/Actividad6P1/src/actividad6p1/Actividad6P1.java
@@ -0,0 +1,22 @@
+package actividad6p1;
+
+/**
+ *
+ * @author Samuel Vergara Martín
+ */
+public class Actividad6P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final int maxUnicodeNumber = 65535;
+        char character = maxUnicodeNumber;
+        Character test = maxUnicodeNumber;
+        Integer testn = 123;
+        String hex = String.format("%04x", (int) 'a');
+        
+        
+    }
+    
+}
