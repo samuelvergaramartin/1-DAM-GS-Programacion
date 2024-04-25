120e96a91405ec03716e6770118bbd93e5e25e70
samuelvergaramartin <accesopuerta0@gmail.com>
2024-04-25 08:50:46 +0200

    Updated main branch

--- Tercer Trimestre/Tema-10/Ejercicios/Ejercicios de apliacion/Actividad10P14Aplicacion/src/Main.java
+++ Tercer Trimestre/Tema-10/Ejercicios/Ejercicios de apliacion/Actividad10P14Aplicacion/src/Main.java
@@ -1,8 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * @author Samuel Vergara Martín
  */
 public class Main {
     public static void main(String[] args) {
-        
+        final String directory = "FicherosActividad10P14Aplicacion/";
+        Scanner sc;
+        int nCaracteres = 0;
+
+        try (BufferedReader in = new BufferedReader(new FileReader(directory + "carta.txt"))){
+            int c = in.read();
+            while(c != -1) {
+                nCaracteres++;
+                c = in.read();
+            };
+        }
+        catch (IOException exception){
+            System.out.println(exception);
+        }
+
+        System.out.println(nCaracteres);
     }
 }
