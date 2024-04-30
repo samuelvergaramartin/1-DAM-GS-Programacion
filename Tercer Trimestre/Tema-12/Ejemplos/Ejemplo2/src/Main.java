74431b2ee273394657076e85ff5fcbed5e3bf697
samuelvergaramartin <accesopuerta0@gmail.com>
2024-04-30 09:59:15 +0200

    Updated main branch

--- Tercer Trimestre/Tema-12/Ejemplos/Ejemplo2/src/Main.java
+++ Tercer Trimestre/Tema-12/Ejemplos/Ejemplo2/src/Main.java
@@ -3,22 +3,14 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente clientes[] = {
-          new Cliente("1", "Pepe"),
-          new Cliente("2", "Rosa"),
-          new Cliente("20", "Carlos"),
-          new Cliente("10", "Maria")
-        };
+        Object o;
+        Cliente c = new Cliente("1", "Pepe");
 
-        Comparator<Cliente> orden = Comparator.naturalOrder();
+        o = c;
 
-        Arrays.sort(clientes, orden.reversed());
-        System.out.println(Arrays.toString(clientes));
+        Contenedor<?> co;
+        Contenedor<Integer> ci = new Contenedor<>();
 
-        Arrays.sort(clientes, new ComparaNombres());
-        System.out.println(Arrays.toString(clientes));
-
-        Arrays.sort(clientes, new ComparaNombres().reversed());
-        System.out.println(Arrays.toString(clientes));
+        co = ci;
     }
 }
