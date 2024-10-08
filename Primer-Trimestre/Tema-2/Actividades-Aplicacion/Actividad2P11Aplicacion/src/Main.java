f4d58088c5bf4df49d29760d4917e430350cf19e
samuelvergaramartin <accesopuerta0@gmail.com>
2024-10-08 23:17:42 +0200

    Updated main branch

---
+++ Primer-Trimestre/Tema-2/Actividades-Aplicacion/Actividad2P11Aplicacion/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Declaramos las variables
+        int num;
+        Scanner sc = new Scanner(System.in);
+
+        //Pedimos el numero al usuario
+        System.out.print("");
+    }
+}
--- Primer-Trimestre/Tema-2/deberes.md
+++ Primer-Trimestre/Tema-2/deberes.md
@@ -1,3 +1,7 @@
  Ejercicios de la relacion de ejercicios de refuerzo:
 
 - 1 al 3 , 8 y 14.
+
+Ejercicios de aplicacion
+
+2.11
