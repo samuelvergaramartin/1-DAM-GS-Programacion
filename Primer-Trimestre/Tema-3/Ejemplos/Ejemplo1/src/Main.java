6d37f3d578a894832d0d366e527f6538ca0ba4b1
samuelvergaramartin <accesopuerta0@gmail.com>
2024-10-23 13:46:05 +0200

    Updated main branch

--- Primer-Trimestre/Tema-3/Ejemplos/Ejemplo1/src/Main.java
+++ Primer-Trimestre/Tema-3/Ejemplos/Ejemplo1/src/Main.java
@@ -2,10 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        while (mcd>0 && (a%mcd!=0 || b%mcd!=0)){
-            mcd--;
+        int a, b, menor, mcd;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce un número: ");
+        a = sc.nextInt();
+
+        System.out.print("Introduce un número: ");
+        b = sc.nextInt();
+
+        // Calculo el menor
+        menor = (a<b) ? a : b;
+
+        // Hallo el mcd
+        mcd = 1;
+        while (menor>0 && mcd==1){
+            if (a%menor==0 && b%menor==0){
+                mcd = menor;
+            }
+            menor--;
         }
 
-        while (!(mcd <=0 || (a%mcd==0 && b%mcd==0)))
+        System.out.println("El MCD es " + mcd);
     }
 }
