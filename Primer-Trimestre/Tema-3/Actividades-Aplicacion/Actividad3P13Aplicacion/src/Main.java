bb03f387bacc14a5f7b0c24dcf7978295c08315c
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2024-11-05 11:19:29 +0100

    Updated main branch

--- Primer-Trimestre/Tema-3/Actividades-Aplicacion/Actividad3P13Aplicacion/src/Main.java
+++ Primer-Trimestre/Tema-3/Actividades-Aplicacion/Actividad3P13Aplicacion/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int hora, minutos, segundos, segundosAIncrementar;
+        boolean bloqSeg = false;
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Introduce la hora: ");
@@ -17,9 +18,60 @@ public class Main {
         System.out.println("Introduce el número de segundos a incrementar: ");
         segundosAIncrementar = sc.nextInt();
 
-        do {
-
+        if(hora >= 0 && hora <= 23) {
+            if(minutos >= 0 && minutos <= 59) {
+                if(segundos >= 0 && segundos <= 59) {
+                    if(segundos + 1 > 59) {
+                        segundos = 0;
+                        minutos++;
+                        bloqSeg = true;
+                    }
+                    if(minutos > 59) {
+                        minutos = 0;
+                        hora++;
+                    }
+                    if(hora > 23) {
+                        hora = 0;
+                    }
+                    segundosAIncrementar--;
+                    if(!bloqSeg) segundos++;
+                }
+                else {
+                    System.out.println("Error: Formato de segundos incorrecto");
+                    segundosAIncrementar = 0;
+                }
+            }
+            else {
+                System.out.println("Error: Formato de minutos incorrecto");
+                segundosAIncrementar = 0;
+            }
         }
-        while ()
+        else {
+            System.out.println("Error: Formato de la hora incorrecto.");
+            segundosAIncrementar = 0;
+        }
+
+        bloqSeg = false;
+
+        while (segundosAIncrementar > 0) {
+            if(segundos + 1 > 59) {
+                segundos = 0;
+                minutos++;
+                bloqSeg = true;
+            }
+            if(minutos > 59) {
+                minutos = 0;
+                hora++;
+            }
+            if(hora > 23) {
+                hora = 0;
+            }
+            if(!bloqSeg) segundos++;
+
+            segundosAIncrementar--;
+            bloqSeg = false;
+        }
+
+        System.out.println(hora + ":" + minutos + ":" + segundos);
     }
 }
---
+++ Primer-Trimestre/Tema-3/Actividades-Aplicacion/Actividad3P14Aplicacion/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num, primos = 0;
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
