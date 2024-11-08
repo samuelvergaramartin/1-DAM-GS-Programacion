7f953bdde76be27a5e8f84f57c26860e034187f5
Sam170703dev <samuelvergaramartin@protonmail.com>
2024-11-08 08:42:05 +0100

    Updated main branch

--- Primer-Trimestre/Tema-3/Actividades-Aplicacion/Actividad3P14Aplicacion/src/Main.java
+++ Primer-Trimestre/Tema-3/Actividades-Aplicacion/Actividad3P14Aplicacion/src/Main.java
@@ -7,17 +7,18 @@ public class Main {
 
         System.out.print("Introduce un número: ");
         num = sc.nextInt();
-        //Revisar en casa
+
         for(int i = 1; i <= num; i++) {
             contador = 0;
             for(int j = 1; j <= i; j++) {
-                if(i%j == 0) {
-                    contador++;
-                    System.out.println(j + "--> Primo");
-                }
-                else System.out.println(j + "--> No primo");
+                if(i%j == 0) contador++;
             }
-            if(contador <= 2) primos++;
+
+            if(contador <= 2) {
+                primos++;
+                System.out.println(i + "--> Primo");
+            }
+            else System.out.println(i + "--> No primo");
         }
 
         System.out.println("Hay un total de " + primos + " números primos.");
