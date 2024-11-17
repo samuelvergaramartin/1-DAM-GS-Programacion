2e9a3d91b5c00af360627051786d9e386308ace0
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2024-11-17 15:00:35 +0100

    Updated main branch

--- Primer-Trimestre/Tema-4/Actividades-Resueltas/Actividad4P7Resuelta/src/Main.java
+++ Primer-Trimestre/Tema-4/Actividades-Resueltas/Actividad4P7Resuelta/src/Main.java
@@ -2,4 +2,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    static boolean esPrimo(int num) {
+        int contador = 0;
+        boolean resultado = false;
+
+
+        for(int j = 1; j <= num; j++) {
+            if(num%j == 0) contador++;
+        }
+
+        if(contador <= 2) resultado = true;
+
+        return resultado;
+    }
 }
