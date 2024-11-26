18a1a2867860916fb04a46fcbf6601b69d601675
samuelvergaramartin <accesopuerta0@gmail.com>
2024-11-26 13:39:44 +0100

    Updated main branch

--- Primer-Trimestre/Tema-4/Actividades-Resueltas/Actividad4P9Resuelta/src/Main.java
+++ Primer-Trimestre/Tema-4/Actividades-Resueltas/Actividad4P9Resuelta/src/Main.java
@@ -2,13 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int num1, num2, num3;
+        int num1, num2, num3, resultado;
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.print("Introduce un numero: ");
+        num1 = sc.nextInt();
+
+        System.out.print("Introduce otro numero: ");
+        num2 = sc.nextInt();
+
+        System.out.print("Introduce otro numero: ");
+        num3 = sc.nextInt();
+
+        resultado = maximo(num1, num2, num3);
+
+        System.out.println("El máximo es: " + resultado);
     }
 
     private static int maximo(int num1, int num2, int num3) {
+        int resultado;
 
+        resultado = Math.max(num1, num2);
+        resultado = Math.max(resultado, num3);
+
+        return resultado;
     }
 }
