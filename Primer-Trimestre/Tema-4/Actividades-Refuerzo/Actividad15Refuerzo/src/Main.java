4ca0c16369c9ee542b69de1b40009ff2243ca781
samuelvergaramartin <accesopuerta0@gmail.com>
2024-11-28 12:55:28 +0100

    Updated main branch

--- Primer-Trimestre/Tema-4/Actividades-Refuerzo/Actividad15Refuerzo/src/Main.java
+++ Primer-Trimestre/Tema-4/Actividades-Refuerzo/Actividad15Refuerzo/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for(int i = 1; i <= 1000; i++) {
+            if(esPrimo(i)) System.out.println(i);
+        }
+    }
+
+    private static boolean esPrimo(int num) {
+        int contador = 0;
+
+        for(int i = num; i > 0; i--) {
+            if(num % i == 0) contador++;
+        }
+
+        if(contador > 2) return false;
+        else return true;
     }
 }
