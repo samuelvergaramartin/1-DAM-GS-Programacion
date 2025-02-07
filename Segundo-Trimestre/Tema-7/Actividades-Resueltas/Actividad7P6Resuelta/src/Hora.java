eddb1b17ce8d8492c2275bd2eedd786a0f5df4b9
samuelvergaramartin <accesopuerta0@gmail.com>
2025-02-07 11:12:43 +0100

    Updated main branch

---
+++ Segundo-Trimestre/Tema-7/Actividades-Resueltas/Actividad7P6Resuelta/src/Hora.java
@@ -0,0 +1,29 @@
+public class Hora {
+    private int horas;
+    private int minutos;
+    private int segundos;
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
+    }
+}
--- Segundo-Trimestre/Tema-7/Actividades-Resueltas/Actividad7P6Resuelta/src/Main.java
+++ Segundo-Trimestre/Tema-7/Actividades-Resueltas/Actividad7P6Resuelta/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        Hora tiempo = new Hora();
+
+        System.out.print("Horas: ");
+        tiempo.setHoras(sc.nextInt());
+
+        System.out.print("Minutos: ");
+        tiempo.setMinutos(sc.nextInt());
+
+        System.out.print("Segundos: ");
+        tiempo.setSegundos(sc.nextInt());
     }
 }
