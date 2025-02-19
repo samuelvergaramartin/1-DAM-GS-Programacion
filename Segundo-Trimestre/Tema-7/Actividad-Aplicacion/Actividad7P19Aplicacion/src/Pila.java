0bde8f983be8325d81b499d3a66fc42d848b1ab1
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-02-19 22:02:36 +0100

    Updated main branch

---
+++ Segundo-Trimestre/Tema-7/Actividad-Aplicacion/Actividad7P19Aplicacion/src/Pila.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Pila {
+    private Integer[] tabla;
+
+    public Pila() {
+        tabla = new Integer[0];
+    }
+
+    public void apilar(int elemento) {
+        tabla = Arrays.copyOf()
+    }
+}
