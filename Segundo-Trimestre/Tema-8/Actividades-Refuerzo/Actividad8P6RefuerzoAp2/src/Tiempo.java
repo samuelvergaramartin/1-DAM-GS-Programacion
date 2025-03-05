7e8b54e4cd1e25a54e28204a01814b729942a8a5
samuelvergaramartin <accesopuerta0@gmail.com>
2025-03-05 12:54:28 +0100

    Updated main branch

---
+++ Segundo-Trimestre/Tema-8/Actividades-Refuerzo/Actividad8P6RefuerzoAp2/src/Tiempo.java
@@ -0,0 +1,48 @@
+public class Tiempo {
+    private int horas, minutos, segundos;
+
+    public Tiempo(int horas, int minutos, int segundos) {
+        setHoras(horas);
+        setMinutos(minutos);
+        setSegundos(segundos);
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        if(minutos < 0 || minutos > 59) System.out.println("Error: El numero de minutos debe estar entre 0 y 59");
+        else this.minutos = minutos;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        if(horas < 0) System.out.println("Error: El número de horas no puede ser menor que 0.");
+        else this.horas = horas;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(int segundos) {
+        if(segundos < 0 || segundos > 59) System.out.println("Error: El numero de segundos debe estar entre 0 y 59");
+        else this.segundos = segundos;
+    }
+
+    public void suma(Tiempo t) {
+        int h = horas, m = minutos, s = segundos;
+
+        s+=t.segundos;
+        if (s > 60)
+    }
+
+    @Override
+    public String toString() {
+        return horas + "h " + minutos + "m " + segundos + "s";
+    }
+}
