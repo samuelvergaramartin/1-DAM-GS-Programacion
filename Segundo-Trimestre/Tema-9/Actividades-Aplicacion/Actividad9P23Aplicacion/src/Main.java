19d41cfb95802b069e8ceb1b782beff5c1b2e002
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-03-20 14:45:42 +0100

    Updated main branch

--- Segundo-Trimestre/Tema-9/Actividades-Aplicacion/Actividad9P23Aplicacion/src/Main.java
+++ Segundo-Trimestre/Tema-9/Actividades-Aplicacion/Actividad9P23Aplicacion/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Socio socios[] = {
+                new Socio(1, "B", "10-10-2010"),
+                new Socio(2, "A", "10-10-2010"),
+                new Socio(3, "C", "10-10-2010")
+        };
+        ComparaNombres comparaNombres = new ComparaNombres();
+
+        System.out.println("Array sin ordenar");
+        System.out.println(Arrays.toString(socios));
+        System.out.println("Array ordenado a la inversa");
+        Arrays.sort(socios, comparaNombres.reversed());
+        System.out.println(Arrays.toString(socios));
     }
 }
