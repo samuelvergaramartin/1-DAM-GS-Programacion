494b54e1fb4f4ea55e3f47a9ad1337d16556e66a
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-03-25 18:10:43 +0100

    Updated main branch

--- Tercer-Trimestre/Tema-12/Actividades-Resueltas/Actividad12P2Resuelta/src/Contenedor.java
+++ Tercer-Trimestre/Tema-12/Actividades-Resueltas/Actividad12P2Resuelta/src/Contenedor.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Contenedor <T> {
     T lista[];
 
@@ -5,5 +7,47 @@ public class Contenedor <T> {
         this.lista = lista;
     }
 
-    public void insertarAlPrincipio(T nuevo)
+    public void insertarAlPrincipio(T nuevo) {
+        lista = Arrays.copyOf(lista, lista.length + 1);
+        System.arraycopy(lista, 0, lista, 1, lista.length - 1);
+
+        lista[0] = nuevo;
+    }
+
+    public void insertarAlFinal(T nuevo) {
+        lista = Arrays.copyOf(lista, lista.length + 1);
+        lista[lista.length - 1] = nuevo;
+    }
+
+    public T extraerDelPrincipio() {
+        T elemento = null;
+
+        if(lista.length > 0) {
+            elemento = lista[0];
+            System.arraycopy(lista, 1, lista, 0, lista.length - 1);
+            lista = Arrays.copyOf(lista, lista.length - 1);
+        }
+
+        return elemento;
+    }
+
+    public T extraerDelFinal() {
+        T elemento = null;
+
+        if(lista.length > 0) {
+            elemento = lista[lista.length - 1];
+            lista = Arrays.copyOf(lista, lista.length - 1);
+        }
+
+        return elemento;
+    }
+
+    public void ordenar() {
+        Arrays.sort(lista);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(lista);
+    }
 }
--- Tercer-Trimestre/Tema-12/Actividades-Resueltas/Actividad12P2Resuelta/src/Main.java
+++ Tercer-Trimestre/Tema-12/Actividades-Resueltas/Actividad12P2Resuelta/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        Contenedor<Integer> contenedor = new Contenedor<>(new Integer[0]);
 
+        System.out.println(contenedor);
+        contenedor.insertarAlFinal(1);
+        System.out.println(contenedor);
+        contenedor.insertarAlPrincipio(2);
+        System.out.println(contenedor);
+        contenedor.insertarAlPrincipio(3);
+        System.out.println(contenedor);
+        System.out.println(contenedor.extraerDelFinal());
+        System.out.println(contenedor);
+        System.out.println(contenedor.extraerDelPrincipio());
+        System.out.println(contenedor);
     }
 }
