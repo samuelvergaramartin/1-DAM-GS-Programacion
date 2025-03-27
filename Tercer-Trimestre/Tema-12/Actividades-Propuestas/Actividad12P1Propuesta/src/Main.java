dbcfad89ec91be8941b3ac73524e23845f3d0211
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-03-27 14:10:34 +0100

    Updated main branch

--- Tercer-Trimestre/Tema-12/Actividades-Propuestas/Actividad12P1Propuesta/src/Main.java
+++ Tercer-Trimestre/Tema-12/Actividades-Propuestas/Actividad12P1Propuesta/src/Main.java
@@ -4,17 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Integer t1[] = {1,2,3}, t2[] = {4,5,6,7}, resultado[];
 
-        resultado = concatena(t1, t2, new Integer[t1.length + t2.length]);
+        resultado = concatena(t1, t2);
 
         System.out.println(Arrays.toString(resultado));
     }
 
-    private static <T> T[] concatena(T[] tabla1, T[] tabla2, T[] tablaBaseResultado) {
-        if(tablaBaseResultado.length == tabla1.length + tabla2.length) {
-            System.arraycopy(tabla1, 0, tablaBaseResultado, 0, tabla1.length);
-            System.arraycopy(tabla2, 0, tablaBaseResultado, tabla1.length, tabla2.length);
-        }
+    private static <T> T[] concatena(T[] tabla1, T[] tabla2) {
+        T[] resultado = Arrays.copyOf(tabla1, tabla1.length + tabla2.length);
 
-        return tablaBaseResultado;
+        System.arraycopy(tabla2, 0, resultado, tabla1.length, tabla2.length);
+
+        return resultado;
     }
 }
