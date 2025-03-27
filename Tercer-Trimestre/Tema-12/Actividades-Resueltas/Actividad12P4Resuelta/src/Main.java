5aa24ceda29b3079927a51cd89cb649a59cdffb1
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-03-27 13:47:04 +0100

    Updated main branch

---
+++ Tercer-Trimestre/Tema-12/Actividades-Resueltas/Actividad12P4Resuelta/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Collection<Integer> numeros = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        Iterator<Integer> it;
+        int num;
+
+        System.out.print("Introduce un número: ");
+        num = sc.nextInt();
+
+        while (num != -1) {
+            numeros.add(num);
+            System.out.print("Introduce un número: ");
+            num = sc.nextInt();
+        }
+
+        System.out.println("Mostrando valores pares...");
+        for(Integer e : numeros) {
+            if(e%2 == 0) System.out.println(e);
+        }
+
+        System.out.println("Eliminando los impares...");
+
+        it = numeros.iterator();
+
+        while (it.hasNext()) {
+            if(it.next() %2 != 0) it.remove();
+        }
+
+        System.out.println("Mostrando Colección resultante...");
+        System.out.println(numeros);
+    }
+}
