82f21ccaf0060ae4fac7ce988151b52f72331ee8
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-03-28 11:11:54 +0100

    Updated main branch

---
+++ Tercer-Trimestre/Tema-12/Actividades-Propuestas/Actividad12P5Propuesta/src/Cliente.java
@@ -0,0 +1,47 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Cliente implements Comparable<Cliente>{
+    private final String DNI;
+    private String nombre;
+    private final LocalDate FECHA_NACIMIENTO;
+
+    public Cliente(String DNI, String nombre, String FECHA_NACIMIENTO) {
+        this.DNI = DNI;
+        setNombre(nombre);
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd-MM-yy");
+        this.FECHA_NACIMIENTO = LocalDate.parse(FECHA_NACIMIENTO, f);
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFECHA_NACIMIENTO() {
+        return FECHA_NACIMIENTO;
+    }
+
+    public int edad() {
+        return (int) FECHA_NACIMIENTO.until(LocalDate.now(), ChronoUnit.YEARS);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " {\n\tNombre: " + nombre + "\n\tDNI: " + DNI + "\n\tEdad: " + edad() + "\n}";
+    }
+
+    @Override
+    public int compareTo(Cliente o) {
+        return 0;
+    }
+}
