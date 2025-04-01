19f35e047a56f28ca5ad35895168e2f050d20c0c
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-04-01 19:22:11 +0200

    Updated main branch

---
+++ Tercer-Trimestre/Tema-12/Actividades-Resueltas/Actividad12P11Resuelta/src/Contenedor.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Contenedor <T> {
+    private List<T> contenedor;
+
+    public Contenedor() {
+        contenedor = new ArrayList<>();
+    }
+
+    public boolean insertarElemento(T elemento) {
+        boolean insertado = false;
+
+        if(!contenedor.contains(elemento)) {
+            contenedor.add(elemento);
+            insertado = true;
+        }
+
+        return insertado;
+    }
+
+    public T obtenerElemento(int indice) {
+        T elemento = null;
+
+        if (indice < contenedor.size() && indice >= 0) {
+            elemento = contenedor.get(indice);
+        }
+        return elemento;
+    }
+
+    public boolean eliminarElemento(int indice) {
+        boolean eliminado = false;
+
+        if (indice < contenedor.size() && indice >= 0) {
+            if(contenedor.contains(obtenerElemento(indice))) {
+                eliminado = true;
+            }
+        }
+
+        return eliminado;
+    }
+
+    public boolean modificarElemento(int indice, T elemento) {
+        boolean modificado = false;
+
+        if (indice < contenedor.size() && indice >= 0) {
+            contenedor.set(indice, elemento);
+        }
+
+        return modificado;
+    }
+
+    public int obtenerNumeroElementos() {
+        return contenedor.size();
+    }
+
+    public void ordenar(Comparator<T> comparator) {
+        contenedor.sort(comparator);
+    }
+
+    @Override
+    public String toString() {
+        return contenedor.toString();
+    }
+}
--- Tercer-Trimestre/Tema-12/Actividades-Resueltas/Actividad12P11Resuelta/src/Socio.java
+++ Tercer-Trimestre/Tema-12/Actividades-Resueltas/Actividad12P11Resuelta/src/Socio.java
@@ -1,7 +1,8 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
-public class Socio {
+public class Socio implements Comparable<Socio> {
     private final String DNI;
     private String nombre;
     private final LocalDate FECHA_ALTA;
@@ -13,7 +14,36 @@ public class Socio {
         this.FECHA_ALTA = LocalDate.parse(FECHA_ALTA, f);
     }
 
+    public int antiguedad() {
+        return (int) LocalDate.now().until(FECHA_ALTA, ChronoUnit.YEARS);
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public String getFECHA_ALTA() {
+        return FECHA_ALTA.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " {\n\tNombre: " + nombre + "\n\tDNI: " + DNI + "\n\tFecha de Alta: " + FECHA_ALTA + "\n}";
+    }
+
+    @Override
+    public int compareTo(Socio o) {
+        return DNI.compareTo(o.DNI);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Socio)) return false;
+
+        return DNI.equals(((Socio) obj).DNI);
     }
 }
