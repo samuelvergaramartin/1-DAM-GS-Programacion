72514d73282ee8b2a6759e07da7aecc32c1a5443
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-04-09 13:04:39 +0200

    Updated main branch

--- Tercer-Trimestre/Tema-12/Actividades-Aplicacion/Actividad12P20Aplicacion/src/Main.java
+++ Tercer-Trimestre/Tema-12/Actividades-Aplicacion/Actividad12P20Aplicacion/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SuperCola<Integer> superCola = new SuperCola<>();
+        int opcion, numero;
+
+        do {
+            System.out.print();
+        }
+        while (opcion != 5);
+    }
+
+    private static int menu() {
+        Scanner sc = new Scanner(System.in);
+        final String MENU = """
+                Menu
+                1- Encolar en cola1
+                2- Encolar en cola2
+                3- Desencolar de cola1
+                4- Desencolar de cola2
+                5- Salir
+                """;
+        System.out.println(MENU);
+        System.out.print("Seleccione una opción: ");
+
+        return sc.nextInt();
     }
 }
--- Tercer-Trimestre/Tema-12/Actividades-Resueltas/Actividad12P16Resuelta/src/Main.java
+++ Tercer-Trimestre/Tema-12/Actividades-Resueltas/Actividad12P16Resuelta/src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 
 public class Main {
