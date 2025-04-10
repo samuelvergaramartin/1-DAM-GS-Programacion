ce0e02d93a7fbb65c49191bed700293ac06db4d0
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-04-10 14:28:19 +0200

    Updated main branch

--- Tercer-Trimestre/Tema-12/Actividades-Aplicacion/Actividad12P21Aplicacion/src/Main.java
+++ Tercer-Trimestre/Tema-12/Actividades-Aplicacion/Actividad12P21Aplicacion/src/Main.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String cad;
+        Set<Integer> numeros = new TreeSet<>(Comparator.reverseOrder());
 
-        System.out.print("Introduce el texto: ");
-        cad = sc.nextLine();
+        for(int i = 0; i < 20; i++) {
+            numeros.add((int)(Math.random() * 100));
+        }
 
-        System.out.println(cad.toLowerCase());
+        System.out.println(numeros);
     }
 }
