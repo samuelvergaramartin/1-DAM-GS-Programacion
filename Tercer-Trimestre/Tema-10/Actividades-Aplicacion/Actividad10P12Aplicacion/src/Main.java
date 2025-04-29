0d484490770eb246f9f7a00a48dcbc12cdc61b65
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-04-29 20:13:51 +0200

    Updated main branch

--- Tercer-Trimestre/Tema-10/Actividades-Aplicacion/Actividad10P12Aplicacion/src/Main.java
+++ Tercer-Trimestre/Tema-10/Actividades-Aplicacion/Actividad10P12Aplicacion/src/Main.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        BufferedReader in;
+        String nombre;
+        int edad;
+
+        System.out.print("Ingrese su nombre: ");
+        nombre = sc.nextLine();
+
+        do {
+            System.out.print("Ingrese su edad: ");
+            edad = sc.nextInt();
+        }
+        while (edad <= 0);
+
+
     }
 }
