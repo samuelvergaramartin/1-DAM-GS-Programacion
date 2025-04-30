8f305bbe8f63c56f4c3f35a1c9854c38a823e819
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-04-30 20:10:40 +0200

    Updated main branch

--- Tercer-Trimestre/Tema-10/Actividades-Aplicacion/Actividad10P14Aplicacion/src/Main.java
+++ Tercer-Trimestre/Tema-10/Actividades-Aplicacion/Actividad10P14Aplicacion/src/Main.java
@@ -1,6 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         int contCaracteres = 0, contLineas = 0, contPalabras = 0;
-        
+        String contenidoCarta = "";
+
+        try(BufferedReader in = new BufferedReader(new FileReader("Actividad10P14Aplicacion/carta.txt"))) {
+            
+        }
+        catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
