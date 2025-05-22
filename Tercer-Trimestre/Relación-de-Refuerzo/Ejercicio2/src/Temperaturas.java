e7cb52661b648b5661b1f62e9b081aea49ac808a
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-05-22 12:01:16 +0200

    Updated main branch

--- Tercer-Trimestre/Relación-de-Refuerzo/Ejercicio2/src/Temperaturas.java
+++ Tercer-Trimestre/Relación-de-Refuerzo/Ejercicio2/src/Temperaturas.java
@@ -2,11 +2,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Temperaturas {
-    private static Map<String, Integer> temperaturas = new LinkedHashMap<String, Integer>();
+    private static Map<String, Integer> temperaturas = new LinkedHashMap<>();
 
     public static boolean introducirTemperatura(String mes, Integer temperatura) {
         if(temperaturas.containsValue(temperatura)) {
-
+            
         }
     }
 
