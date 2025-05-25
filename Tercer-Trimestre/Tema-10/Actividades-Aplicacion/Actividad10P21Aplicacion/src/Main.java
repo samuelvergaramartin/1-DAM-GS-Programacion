714349731ec5db024d22bbdc8fb7cdd5e8cd9c64
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-05-25 12:54:28 +0200

    Updated main branch

--- Tercer-Trimestre/Tema-10/Actividades-Aplicacion/Actividad10P21Aplicacion/src/Main.java
+++ Tercer-Trimestre/Tema-10/Actividades-Aplicacion/Actividad10P21Aplicacion/src/Main.java
@@ -2,4 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    private static int menu() {
+        final String MENU = """
+                Menú de opciones
+                1- 
+                """;
+    }
 }
